e49c2285ebcedcc9091ed9ae87fcc8ca1b4f9701
patsicha <patsichat@gmail.com>
2014-03-15 18:49:36 +0700

    commit at StarBucks

--- FITNY/TimerViewController.m
+++ FITNY/TimerViewController.m
@@ -360,7 +360,7 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+    static NSString *CellIdentifier = @"Cell2";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
     // Configure the cell...
